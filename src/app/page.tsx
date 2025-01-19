6dee5b48a4f47148f13f14a673b85f9e1048f9ba
jaweriaawan <akhterwaheed470@gmail.com>
2025-01-19 14:07:31 +0500

    changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import { client } from "@/sanity/lib/client"
+import { urlFor } from "@/sanity/lib/image"
 import Image from "next/image"
 
 export default async function FetchProducts(){
@@ -18,8 +19,8 @@ export default async function FetchProducts(){
     {
       FetchData.map((Data : any) => (
         <div key={Data.id} className="w-[550px] h-[730px] hover:shadow-slate-400 hover:shadow-lg shadow-md shadow-slate-300 rounded-lg">
-            <Image src={Data.productImage} alt="image" className="w-[550px] h-[480px]"/>
-              <div className="pl-[20px] pt-[20px] font-sans">
+               <img src={urlFor(Data.productImage).url()} alt="" width={'550px'} height={'480px'} />
+               <div className="pl-[20px] pt-[20px] font-sans">
                 <h1 className="text-[22px] font-semibold">{Data.productName}</h1>
                 <p className="text-[18px] text-gray-400">{Data.category}</p>
                 <p className="text-[19px] text-red-500 font-semibold">{Data.status}</p>
